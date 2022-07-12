3e6e89aa83705d2bfc0ab1fc2fb0a1e8804013d1
Rohan Bhande <whoseunassailable@gmail.com>
2022-07-12 12:46:21 +0530

    Created player one class with its properties successfully

--- src/com/project/intermediate/game/Main.java
+++ src/com/project/intermediate/game/Main.java
@@ -1,4 +1,30 @@
 package com.project.intermediate.game;
 
+/* Problem Statement
+There are two players : let's say player one and player two.
+Player One : Name, weapon and health.
+Player Two : Name, weapon and health + Armour.
+
+For Player One :
+If player gets damaged by a gun one then health is reduced by 30.
+If player gets damaged by a gun two then health is reduced by 50.
+
+For Player Two :
+If player is wearing an armour then :
+    If player gets damaged by gun one then health is reduced by 20.
+    if player gets damaged by gun two then health is reduced by 40.
+
+If player is not wearing an armour then :
+    If player gets damaged by a gun one then health is reduced by 30.
+    If player gets damaged by a gun two then health is reduced by 50.
+
+If we press on heal :
+    Health will reach to 100.
+*/
+
 public class Main {
+    public static void main(String[] args) {
+        //
+        Player playerOne = new Player();
+    }
 }
---
+++ src/com/project/intermediate/game/Player1.java
@@ -0,0 +1,63 @@
+package com.project.intermediate.game;
+
+public class Player1 {
+    private String name;
+    private String weapon;
+    private int health;
+
+    public Player1(String name, String weapon, int health) {
+        this.name = name;
+        this.weapon = weapon;
+        this.health = health;
+    }
+
+    private void damageByGunOne() {
+        this.health -= 30;
+        if (this.health <= 0) {
+            this.health = 0;
+        }
+
+        System.out.println("Got hit by gun 1 and health is reduced by 30. New health is " + this.health);
+
+        if (this.health == 0) {
+            System.out.println(getName() + " is dead.");
+        }
+    }
+
+    private void damageByGunTwo() {
+        this.health -= 50;
+        if (this.health <= 0) {
+            this.health = 0;
+        }
+
+        System.out.println("Got hit by gun 1 and health is reduced by 50. New health is " + this.health);
+
+        if (this.health == 0) {
+            System.out.println(getName() + " is dead.");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+}
